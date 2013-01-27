4965e4799229a35917282a7b9d7f8a421f465c42
casi <xiujun.guo@gmail.com>
2013-01-28 02:04:14 +0800

    add spock unit test

--- casi-commons/src/main/java/com/casi/commons/test/SpockTestCaseBase.groovy
+++ casi-commons/src/main/java/com/casi/commons/test/SpockTestCaseBase.groovy
@@ -1,10 +1,6 @@
 package com.casi.commons.test
 
-import org.junit.runner.RunWith
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.context.ApplicationContext
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import spock.lang.Specification
 
 /**
