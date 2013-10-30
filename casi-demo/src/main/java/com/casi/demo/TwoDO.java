41372e17212839a443cdf42c7303e03f9d4018b0
casi <xiujun.guo@gmail.com>
2013-10-30 21:21:57 +0800

    add jdo moudel

---
+++ casi-demo/src/main/java/com/casi/demo/TwoDO.java
@@ -0,0 +1,38 @@
+package com.casi.demo;
+
+import java.util.Date;
+
+/**
+ * User: Think
+ * Date: 13-10-30
+ * Time: 下午7:47
+ */
+public class TwoDO {
+    private int age;
+    private String name;
+    private Date birthday;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+}
